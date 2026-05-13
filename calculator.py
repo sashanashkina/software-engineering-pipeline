b3de127bbf5b2ed31aa0a967b8748b354b68bfff
SA44rr <aranjosa2006@gmail.com>
2026-05-13 15:29:51 +0200

    Power function
    Chi

--- calculator.py
+++ calculator.py
@@ -1,6 +1,11 @@
 def add(a, b):
-return a + b
+    return a + b
 def sub(a, b):
-return a - b
+    return a - b
 def multiply(a, b):
     return a * b
+def power(a, b):
+    c = 1
+    for i in range(b):
+        c*=a
+    return c
