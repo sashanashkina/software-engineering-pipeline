d0e181105c8aad8b609d3d90294a4d835866a0b2
Marcgom-upf <marc.gomez09@estudiant.upf.edu>
2026-05-13 16:09:20 +0200

    errores de estilo

--- calculator.py
+++ calculator.py
@@ -1,15 +1,24 @@
 def add(a, b):
     return a + b
+
+
 def sub(a, b):
     return a - b
+
+
 def multiply(a, b):
     return a * b
+
+
 def power(a, b):
     c = 1
     for i in range(b):
         c*=a
     return c
+
+
 def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b 
+
